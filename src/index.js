9c16519d5d432188b95e06b68e447088f630ab1a
frankwang <frank@benglan.cc>
2018-01-25 20:57:53 +0800

    can show echart

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
+var echarts = require('echarts');
 
-
-var greet = document.createElement('div');
-greet.textContent = "Hi there and greetings!!";
-document.querySelector("#root").appendChild(greet);
+// 基于准备好的dom，初始化echarts实例
+var myChart = echarts.init(document.getElementById('root'));
+// 绘制图表
+myChart.setOption({
+    title: {
+        text: 'ECharts 入门示例'
+    },
+    tooltip: {},
+    xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+    },
+    yAxis: {},
+    series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+    }]
+});
--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,5 @@ module.exports = {
     output: {
         path: __dirname + "/public",//打包后的文件存放的地方
         filename: "bundle.js"//打包后输出文件的文件名
-    }
+    },
 }
